603c8cb8c4a2f37aff61e6dc97b05860523bd977
gilgiv <gilgiv@gmail.com>
2023-11-27 14:34:28 +0200

    merge files from "files" folder to "target" folder

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
+import PyPDF2
+import sys
+import os
 
-SOURCE_FOLDER = "/files"
-TARGET_FOLDER = "/target"
+SOURCE_FOLDER = "./files"
+TARGET_FOLDER = "./target"
+MERGED_FILE = f"{TARGET_FOLDER}/merged.pdf"
 
+merger = PyPDF2.PdfMerger()
+
+for file in os.listdir(SOURCE_FOLDER):
+    if file.endswith(".pdf"):
+        merger.append(f"{SOURCE_FOLDER}/{file}")
+merger.write(MERGED_FILE)
+print(f"Merged PDF file was created: {MERGED_FILE}")
